986dae77cd26264f301f435b71a85f60a3d9011e
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-06-01 20:22:06 -0400

    Added function to fetch forecast data based on user's city input
    - Created a function that uses OpenWeatherMap Geo API to get latitude and longitude data from the users city input
    - Used the latitude and longitude to make a second API call to fetch forecast data
    - Logged the forecast data to the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,35 @@
 $(function () {
   //create get weather data function and pass in the city
+  var weatherApiKey = "c0c121fba052263fed9243172c4438c8";
   function getWeatherData(city) {
-    var weatherApiKey = "c0c121fba052263fed9243172c4438c8";
-
     $.ajax({
       url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`,
       type: "GET",
       dataType: "json",
       success: function (data) {
         console.log(data);
+      },
+    });
+  }
+  function getForecastData(city) {
+    $.ajax({
+      //get latitude and longitude data using city input and geo api
+      url: `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${weatherApiKey}`,
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+
+        //make another call to use lat and lon to get forecast data
+        $.ajax({
+          url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`,
+          type: "GET",
+          dataType: "json",
+          success: function (data) {
+            console.log(data);
+          },
+        });
       },
     });
   }
@@ -19,5 +40,6 @@ $(function () {
     localStorage.setItem("city", city);
     //call the weather data function
     getWeatherData(city);
+    getForecastData(city);
   });
 });
