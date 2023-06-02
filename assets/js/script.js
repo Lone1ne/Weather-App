42cc0ac9c1466978872457db24cbd173363bf79d
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-06-01 21:22:31 -0400

    Updated API calls to return specific data(name, temp, icon, wind speed, weather condition and humidity) and then console logged the data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,15 @@ $(function () {
       type: "GET",
       dataType: "json",
       success: function (data) {
+        //grab specific data we want to use
         console.log(data);
+        console.log(`${data.name}`);
+        console.log(`${data.weather[0].main}`);
+        //change temp from kelvins to F
+        console.log(`${((data.main.temp * 9) / 5 - 459.67).toFixed(2)}`);
+        console.log(`${data.weather[0].icon}`);
+        console.log(`${data.wind.speed}`);
+        console.log(`${data.main.humidity}`);
       },
     });
   }
@@ -28,6 +36,18 @@ $(function () {
           dataType: "json",
           success: function (data) {
             console.log(data);
+            //loop through data list and get forecast for 5 days
+            //since our endpoint is 3-hour 5 day, we get back 40 data points. in order to return 5 we need to grab every 8
+            for (let i = 0; i < data.list.length; i += 8) {
+              //change temp from kelvins to F
+              console.log(
+                `${((data.list[i].main.temp * 9) / 5 - 459.67).toFixed(2)}`
+              );
+              console.log(`${data.list[i].weather[0].icon}`);
+              console.log(`${data.list[i].wind.speed}`);
+              console.log(`${data.list[i].main.humidity}`);
+              console.log(`${data.list[i].dt_txt}`);
+            }
           },
         });
       },
